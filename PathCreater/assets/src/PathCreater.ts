4b1caa2086f15abb96f5d6049584cc853fc4101b
xiayangqun <xyq377x@163.com>
2025-09-22 14:31:47 +0800

    save

--- PathCreater/assets/src/PathCreater.ts
+++ PathCreater/assets/src/PathCreater.ts
@@ -41,7 +41,7 @@ export class PathCreateCtr extends Component {
         this.createMeshData();
 
     }
-    private _pointCount: number = 10;
+    private _pointCount: number = 0;
 
     //除去开头和结尾的长度，每个路径点之间的直线长度。
     //此值越大，则路径越不平滑
@@ -68,6 +68,7 @@ export class PathCreateCtr extends Component {
             return;
         }
 
+        //先生成指定数量的路径点.
         let children = this.pathPointContainer.children;
         let currentLength = children.length;
         let needLength = newValue + 1; // 包括起点
@@ -83,6 +84,7 @@ export class PathCreateCtr extends Component {
             }
         }
 
+        //由 P0 到 P1的路径段l(0,1)为直线段，长度为 firstSegmentLength
         children = this.pathPointContainer.children;
         children[0].setPosition(0, 0, 0);
         children[1].setPosition(0, 0, -this.firstSegmentLength);
@@ -90,7 +92,7 @@ export class PathCreateCtr extends Component {
         let createDirecPathArray = (fromIndex: number, count: number) => {
             for (let j = 0; j < count; j++) {
                 let curIndex = fromIndex + j;
-                createDirecPath(curIndex)
+                createDirecPath(curIndex);
             }
         }
 
@@ -111,7 +113,7 @@ export class PathCreateCtr extends Component {
                 Quat.fromAxisAngle(quat, up, this.slopeAngle * Math.PI / 180);
                 let leftDir = Vec3.transformQuat(new Vec3(), dir, quat);
                 let newPos = Vec3.scaleAndAdd(new Vec3(), children[curIndex - 1].position, leftDir, this.segmentLength);
-                //如果弯度过大，导致当前的点z轴坐标大于前一个点的z轴坐标(路径回头了)，则重新生成该点为直线
+                //如果弯度过大，导致当前的点z轴坐标大于前一个点的z轴坐标(路径回头了)，则重新生成该点为直线，路径点的z应该是越来越小的。因为-Z为前进方向
                 if (newPos.z > children[curIndex - 1].position.z) {
                     createDirecPath(curIndex);
                 }
@@ -141,6 +143,7 @@ export class PathCreateCtr extends Component {
             }
         }
 
+        //当当前路径点设置为比前一个路径点在竖直方向上抬高angle度。
         let createPitchPath = (fromIndex: number, angle: number) => {
             let dir = Vec3.subtract(new Vec3(), children[fromIndex - 1].position, children[fromIndex - 2].position);
             dir = dir.normalize();
@@ -162,6 +165,7 @@ export class PathCreateCtr extends Component {
                     createPitchPath(fromIndex + l, -slopAngle); //15
                 }
             }
+            //等价于
             // createPitchPath(fromIndex + 1, 15); //15
             // createPitchPath(fromIndex + 2, 15); //30
             // createPitchPath(fromIndex + 3, 15); //45
@@ -179,6 +183,7 @@ export class PathCreateCtr extends Component {
                     createPitchPath(fromIndex + l, +slopAngle); //15
                 }
             }
+            //等价于
             // createPitchPath(fromIndex + 1, -15); //-15
             // createPitchPath(fromIndex + 2, -15); //-30
             // createPitchPath(fromIndex + 3, -15); //-45
@@ -188,9 +193,17 @@ export class PathCreateCtr extends Component {
         }
 
         let i = 2;
+        //爬坡或者下坡需要的路径点数量，假设我们的坡度变化为15度，最大坡度为45度，则需要7个路径点来完成一个完整的上坡和下坡
+        // 0 15 30 45 30 15 0
+        //如果坡度变化为10度，则需要10个路径点来完成一个完整的上坡和下坡
+        // 0 10 20 30 40 50 40 30 20 10 0
         let slopeCount = Math.floor(this.maxSlopeAngle / this.slopeAngle) * 2 + 1;
+
+        //单次转弯允许的最大段数，这里是为了防止一次转弯就累计到了90度，让路径点回头了
         let maxTurnCount = Math.floor(90 / this.slopeAngle);
 
+        //路径点的最低高度和最高高度，如果上坡之前发现当前高度已经达到最高了，就不会再上坡了
+        //同理，下坡也是一样的道理
         let minHight = 0 + 1.47;
         let maxHight = 5 - 1.47;
         while (i < needLength - 1) {
@@ -202,6 +215,7 @@ export class PathCreateCtr extends Component {
             let randomType = Math.floor(Math.random() * 5);
             if (randomType === 0) {
                 let dirCectCount = Math.floor(Math.random() * maxTurnCount);
+                //如果剩余的路径点不足，则直接创建剩余的所有路径点为直线
                 if (i + dirCectCount >= needLength - 1) {
                     dirCectCount = needLength - 1 - i;
                 }
@@ -209,6 +223,7 @@ export class PathCreateCtr extends Component {
                 i += dirCectCount;
             }
             else if (randomType === 1) {
+                //左转，随机N个路径点进行连续左转，只要N不超过maxTurnCount就行
                 let turnCount = Math.floor(Math.random() * maxTurnCount);
                 if (i + turnCount >= needLength - 1) {
                     turnCount = needLength - 1 - i;
@@ -217,6 +232,7 @@ export class PathCreateCtr extends Component {
                 i += turnCount;
             }
             else if (randomType === 2) {
+                //右转，随机N个路径点进行连续右转，只要N不超过maxTurnCount就行
                 let turnCount = Math.floor(Math.random() * maxTurnCount);
                 if (i + turnCount >= needLength - 1) {
                     turnCount = needLength - 1 - i;
@@ -225,6 +241,8 @@ export class PathCreateCtr extends Component {
                 i += turnCount;
             }
             else if (randomType === 3) {
+                //如果当前高度已经达到最高了，就不会再上坡了，直接创建直线路径
+                //如果剩余的路径点数量不足以完成一个完整的上坡和下坡，则创建直线路径
                 if (children[i - 1].position.y < maxHight && i + slopeCount < needLength - 1) {
                     createNoseUpPitchPath(i, this.slopeAngle, slopeCount);
                     i += slopeCount;
@@ -234,6 +252,8 @@ export class PathCreateCtr extends Component {
                 }
             }
             else {
+                //如果当前高度已经达到最低了，就不会再下坡了，直接创建直线路径
+                //如果剩余的路径点数量不足以完成一个完整的上坡和下坡，则创建直线路径
                 if (children[i - 1].position.y > minHight && i + slopeCount < needLength - 1) {
                     createNoseDownPitchPath(i, this.slopeAngle, slopeCount);
                     i += slopeCount;
@@ -244,7 +264,7 @@ export class PathCreateCtr extends Component {
             }
         }
 
-
+        //最后一段路径为直线，长度为 lastSegmentLength
         let lastDir = Vec3.subtract(new Vec3(), children[needLength - 2].position, children[needLength - 3].position);
         lastDir = lastDir.normalize();
         let lastPos = Vec3.scaleAndAdd(new Vec3(), children[needLength - 2].position, lastDir, this.firstSegmentLength);
@@ -260,12 +280,10 @@ export class PathCreateCtr extends Component {
             return;
         }
 
-        let vertices: number[] = new Array((currentLength - 1) * 6 * 3);
-        let normals: number[] = new Array((currentLength - 1) * 6 * 3);
-        let indices: number[] = new Array((currentLength - 1) * 2 * 3);
         let leftArray: Array<Vec3> = [];
         let rightArray: Array<Vec3> = [];
 
+        //计算每个路径点的左右顶点位置
         for (let i = 0; i < currentLength; i++) {
             let pathWidth = this.pathWidth;
             let dir: Vec3;
@@ -292,7 +310,11 @@ export class PathCreateCtr extends Component {
             rightArray.push(rightPoint);
         }
 
-        //跑道顶点数据集
+
+        let vertices: number[] = new Array((currentLength - 1) * 6 * 3);
+        let normals: number[] = new Array((currentLength - 1) * 6 * 3);
+        let indices: number[] = new Array((currentLength - 1) * 2 * 3);
+        //顶点数据集
         for (let i = 0; i < currentLength - 1; i++) {
             let verticesStartIndex = i * 6 * 3;
 
@@ -332,12 +354,12 @@ export class PathCreateCtr extends Component {
             }
 
             let leftArrayN = solveNormal(leftArray[i], rightArray[i], leftArray[i + 1]);
-            let rightArrayN = solveNormal(rightArray[i], leftArray[i + 1], leftArray[i]);
-            let leftNextN = solveNormal(leftArray[i + 1], leftArray[i], rightArray[i]);
+            let rightArrayN = leftArrayN//solveNormal(rightArray[i], leftArray[i + 1], leftArray[i]);
+            let leftNextN = leftArrayN//solveNormal(leftArray[i + 1], leftArray[i], rightArray[i]);
 
-            let leftNextN2 = solveNormal(leftArray[i + 1], rightArray[i], rightArray[i + 1]);
-            let rightN2 = solveNormal(rightArray[i], rightArray[i + 1], leftArray[i + 1]);
-            let rightNextN = solveNormal(rightArray[i + 1], leftArray[i + 1], rightArray[i]);
+            let leftNextN2 = leftArrayN;//solveNormal(leftArray[i + 1], rightArray[i], rightArray[i + 1]);
+            let rightN2 =  leftNextN2;//solveNormal(rightArray[i], rightArray[i + 1], leftArray[i + 1]);
+            let rightNextN = leftNextN2;//solveNormal(rightArray[i + 1], leftArray[i + 1], rightArray[i]);
 
             //左边三角形
             normals[verticesStartIndex + 0] = leftArrayN.x;
@@ -391,7 +413,7 @@ export class PathCreateCtr extends Component {
     }
 
     
-    //更加给出的虚拟位置，
+    //根据给出的虚拟位置，将物体放置到跑道上
     public putItemByVirtualPosition (node:Node, vx:number, vz:number): boolean {
         let startPointIndex = Math.floor(vz / this.segmentLength) + 1;
         let endPointIndex = startPointIndex + 1;
